70d9058871173d44571d8eb67f8d4cee1e181212
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-04-09 10:42:54 +0100

    feat: add blank layout

--- newspack-newsletters-templates.php
+++ newspack-newsletters-templates.php
@@ -8,18 +8,34 @@
 add_filter(
 	'newspack_newsletters_templates',
 	function( $templates ) {
+ 		$decode  = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/markup/blank.json'), true ); //phpcs:ignore
+		$content     = $decode['content'];
+		$templates[] = [
+			'content' => $content,
+			'image'   => plugins_url( 'src/templates/images/blank.png', __FILE__ ),
+			'title'   => __( 'Blank', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
 		$decode  = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/markup/template-1.json'), true ); //phpcs:ignore
 		$content = $decode['content'];
 
 		$custom_logo_id = get_theme_mod( 'custom_logo' );
 		$logo           = wp_get_attachment_image( $custom_logo_id, 'full' );
-		
+
 		$sitename = get_bloginfo( 'name' );
-		$content  = str_replace( 
-			[ 
-				'__SITENAME__', 
+		$content  = str_replace(
+			[
+				'__SITENAME__',
 				'__LOGO__',
-			], 
+			],
 			[
 				$sitename,
 				$logo,
