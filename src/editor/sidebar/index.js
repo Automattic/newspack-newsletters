8680ae9ee96d08d5f4b2c41b9261782b37ea67c5
Jefferson Rabb <jeff@atavist.net>
2020-04-12 22:43:52 -0400

    refactor: directory structure change to prepare for multiple components

---
+++ src/editor/sidebar/index.js
@@ -0,0 +1,208 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import apiFetch from '@wordpress/api-fetch';
+import { withSelect, subscribe } from '@wordpress/data';
+import { Component, Fragment } from '@wordpress/element';
+import {
+	Button,
+	ExternalLink,
+	Modal,
+	Notice,
+	SelectControl,
+	Spinner,
+	TextControl,
+} from '@wordpress/components';
+import './style.scss';
+
+class Sidebar extends Component {
+	state = {
+		campaign: {},
+		lists: [],
+		hasResults: false,
+		inFlight: false,
+		isPublishingOrSaving: false,
+		showTestModal: false,
+		testEmail: '',
+		senderEmail: '',
+		senderName: '',
+	};
+	componentDidMount = () => {
+		this.retrieveMailchimp();
+		subscribe( () => {
+			const { isPublishingPost, isSavingPost } = this.props;
+			const { isPublishingOrSaving } = this.state;
+			if ( ( isPublishingPost() || isSavingPost() ) && ! isPublishingOrSaving ) {
+				this.setState( { isPublishingOrSaving: true } );
+			}
+			if ( ! isPublishingPost() && ! isSavingPost() && isPublishingOrSaving ) {
+				this.setState( { isPublishingOrSaving: false }, () => {
+					this.retrieveMailchimp();
+				} );
+			}
+		} );
+	};
+	retrieveMailchimp = () => {
+		const { postId } = this.props;
+		apiFetch( { path: `/newspack-newsletters/v1/mailchimp/${ postId }` } ).then( result =>
+			this.setStateFromAPIResponse( result )
+		);
+	};
+	sendMailchimpTest = () => {
+		const { testEmail } = this.state;
+		this.setState( { inFlight: true } );
+		const { postId } = this.props;
+		const params = {
+			path: `/newspack-newsletters/v1/mailchimp/${ postId }/test`,
+			data: {
+				test_email: testEmail,
+			},
+			method: 'POST',
+		};
+		apiFetch( params ).then( result => this.setStateFromAPIResponse( result ) );
+	};
+	sendMailchimpCampaign = () => {
+		const { senderEmail, senderName } = this.state;
+		this.setState( { inFlight: true } );
+		const { postId } = this.props;
+		const params = {
+			path: `/newspack-newsletters/v1/mailchimp/${ postId }/send`,
+			data: {
+				sender_email: senderEmail,
+				sender_name: senderName,
+			},
+			method: 'POST',
+		};
+		apiFetch( params ).then( result => this.setStateFromAPIResponse( result ) );
+	};
+	setList = listId => {
+		this.setState( { inFlight: true } );
+		const { postId } = this.props;
+		const params = {
+			path: `/newspack-newsletters/v1/mailchimp/${ postId }/list/${ listId }`,
+			method: 'POST',
+		};
+		apiFetch( params ).then( result => this.setStateFromAPIResponse( result ) );
+	};
+	setStateFromAPIResponse = result => {
+		this.setState( {
+			campaign: result.campaign,
+			lists: result.lists.lists,
+			hasResults: true,
+			inFlight: false,
+		} );
+	};
+
+	/**
+	 * Render
+	 */
+	render() {
+		const {
+			campaign,
+			hasResults,
+			inFlight,
+			lists,
+			showTestModal,
+			testEmail,
+			senderName,
+			senderEmail,
+		} = this.state;
+		if ( ! hasResults ) {
+			return [ __( 'Loading Mailchimp data', 'newspack-newsletters' ), <Spinner key="spinner" /> ];
+		}
+		const { recipients, status, long_archive_url } = campaign || {};
+		const { list_id } = recipients || {};
+		if ( ! status ) {
+			return (
+				<Notice status="info" isDismissible={ false }>
+					<p>{ __( 'Publish to sync to Mailchimp', 'newspack-newsletters' ) }</p>
+				</Notice>
+			);
+		}
+		if ( 'sent' === status || 'sending' === status ) {
+			return (
+				<Notice status="info" isDismissible={ false }>
+					<p>{ __( 'Campaign has been sent.', 'newspack-newsletters' ) }</p>
+				</Notice>
+			);
+		}
+		return (
+			<Fragment>
+				<Button
+					isPrimary
+					onClick={ () => this.setState( { showTestModal: true } ) }
+					disabled={ inFlight }
+				>
+					{ __( 'Send Test', 'newspack-newsletters' ) }
+				</Button>
+				{ showTestModal && (
+					<Modal
+						title={ __( 'Send Test Email', 'newspack-newsletters' ) }
+						onRequestClose={ () => this.setState( { showTestModal: false } ) }
+					>
+						<TextControl
+							label={ __( 'Send to this email', 'newspack-newsletters' ) }
+							value={ testEmail }
+							onChange={ value => this.setState( { testEmail: value } ) }
+						/>
+						<Button
+							isPrimary
+							onClick={ () =>
+								this.setState( { showTestModal: false }, () => this.sendMailchimpTest() )
+							}
+						>
+							{ __( 'Send', 'newspack-newsletters' ) }
+						</Button>
+					</Modal>
+				) }
+
+				<SelectControl
+					label={ __( 'Mailchimp Lists', 'newspack-newsletters' ) }
+					value={ list_id }
+					options={ [
+						{
+							value: null,
+							label: __( '-- Select A List --', 'newspack-newsletters' ),
+						},
+						...lists.map( ( { id, name } ) => ( {
+							value: id,
+							label: name,
+						} ) ),
+					] }
+					onChange={ value => this.setList( value ) }
+					disabled={ inFlight }
+				/>
+				<TextControl
+					label={ __( 'Sender name', 'newspack-newsletters' ) }
+					value={ senderName }
+					onChange={ value => this.setState( { senderName: value } ) }
+				/>
+				<TextControl
+					label={ __( 'Sender email', 'newspack-newsletters' ) }
+					value={ senderEmail }
+					onChange={ value => this.setState( { senderEmail: value } ) }
+				/>
+				{ list_id && (
+					<div>
+						<Button isPrimary onClick={ () => this.sendMailchimpCampaign() } disabled={ inFlight }>
+							{ __( 'Send Campaign', 'newspack-newsletters' ) }
+						</Button>
+					</div>
+				) }
+				{ long_archive_url && (
+					<div>
+						<ExternalLink href={ long_archive_url }>
+							{ __( 'View on Mailchimp', 'newspack-newsletters' ) }
+						</ExternalLink>
+					</div>
+				) }
+			</Fragment>
+		);
+	}
+}
+
+export default withSelect( select => {
+	const { getCurrentPostId, isPublishingPost, isSavingPost } = select( 'core/editor' );
+	return { postId: getCurrentPostId(), isPublishingPost, isSavingPost };
+} )( Sidebar );
