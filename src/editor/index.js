43fb115d3c7bd6cdf930a0843a18f6d7dd5ad690
Adam Borowski <adam@adamboro.com>
2020-04-14 12:42:21 +0200

    feat: warn about nested groups

--- src/editor/index.js
+++ src/editor/index.js
@@ -20,7 +20,7 @@ import Editor from './editor/';
 import PrePublishSlot from './pre-publish-slot';
 
 import { addBlocksValidationFilter } from './blocks-validation/blocks-filters';
-import { NestedColumnsDetection } from './blocks-validation/nested-columns-detection';
+import { NestedColumnsDetection } from './blocks-validation/nesting-detection';
 
 addBlocksValidationFilter();
 
