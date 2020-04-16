261f12e1e779b35ca07647e9d8d0b124f6431ac2
Adam Borowski <adam@adamboro.com>
2020-04-16 13:03:00 +0200

    feat: add query controls; move settings to side panel

--- src/editor/blocks/latest-posts/index.js
+++ src/editor/blocks/latest-posts/index.js
@@ -10,8 +10,9 @@ import { registerBlockType, createBlock } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 import { withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
-import { RangeControl, Button, ToggleControl } from '@wordpress/components';
-import { InnerBlocks, BlockPreview } from '@wordpress/block-editor';
+import { RangeControl, Button, ToggleControl, PanelBody } from '@wordpress/components';
+import { InnerBlocks, BlockPreview, InspectorControls } from '@wordpress/block-editor';
+import { Fragment } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -19,49 +20,49 @@ import { InnerBlocks, BlockPreview } from '@wordpress/block-editor';
 import './style.scss';
 import icon from './icon';
 import { getBlocksTemplate } from './utils';
+import QueryControlsSettings from './query-controls';
 
 const LatestPostsBlock = ( { setAttributes, attributes, latestPosts } ) => {
 	const templateBlocks = flatten( getBlocksTemplate( latestPosts, attributes ) );
 	return attributes.isReady ? (
 		<InnerBlocks template={ templateBlocks } />
 	) : (
-		<div className="newspack-latest-posts">
-			<RangeControl
-				label={ __( 'Number of posts', 'newspack-newsletters' ) }
-				value={ attributes.postsToShow }
-				onChange={ value => setAttributes( { postsToShow: value } ) }
-				min={ 1 }
-				max={ 10 }
-			/>
-
-			<ToggleControl
-				label={ __( 'Display post excerpt' ) }
-				checked={ attributes.displayPostExcerpt }
-				onChange={ value => setAttributes( { displayPostExcerpt: value } ) }
-			/>
-			{ attributes.displayPostExcerpt && (
-				<RangeControl
-					label={ __( 'Max number of words in excerpt' ) }
-					value={ attributes.excerptLength }
-					onChange={ value => setAttributes( { excerptLength: value } ) }
-					min={ 10 }
-					max={ 100 }
-				/>
-			) }
-
-			<ToggleControl
-				label={ __( 'Display date' ) }
-				checked={ attributes.displayPostDate }
-				onChange={ value => setAttributes( { displayPostDate: value } ) }
-			/>
-
-			<Button isPrimary onClick={ () => setAttributes( { isReady: true } ) }>
-				{ __( 'Insert', 'newspack-newsletters' ) }
-			</Button>
-			<div className="newspack-latest-posts__preview">
-				<BlockPreview blocks={ templateBlocks.map( template => createBlock( ...template ) ) } />
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={ __( 'Post content settings', 'newspack-newsletters' ) }>
+					<ToggleControl
+						label={ __( 'Display post excerpt', 'newspack-newsletters' ) }
+						checked={ attributes.displayPostExcerpt }
+						onChange={ value => setAttributes( { displayPostExcerpt: value } ) }
+					/>
+					{ attributes.displayPostExcerpt && (
+						<RangeControl
+							label={ __( 'Max number of words in excerpt', 'newspack-newsletters' ) }
+							value={ attributes.excerptLength }
+							onChange={ value => setAttributes( { excerptLength: value } ) }
+							min={ 10 }
+							max={ 100 }
+						/>
+					) }
+					<ToggleControl
+						label={ __( 'Display date', 'newspack-newsletters' ) }
+						checked={ attributes.displayPostDate }
+						onChange={ value => setAttributes( { displayPostDate: value } ) }
+					/>
+				</PanelBody>
+				<PanelBody title={ __( 'Sorting and filtering', 'newspack-newsletters' ) }>
+					<QueryControlsSettings attributes={ attributes } setAttributes={ setAttributes } />
+				</PanelBody>
+			</InspectorControls>
+			<div className="newspack-latest-posts">
+				<Button isPrimary onClick={ () => setAttributes( { isReady: true } ) }>
+					{ __( 'Insert', 'newspack-newsletters' ) }
+				</Button>
+				<div className="newspack-latest-posts__preview">
+					<BlockPreview blocks={ templateBlocks.map( template => createBlock( ...template ) ) } />
+				</div>
 			</div>
-		</div>
+		</Fragment>
 	);
 };
 
