720c979874b755761ce5bd02f1afd5b0183e2bec
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-04-16 11:42:33 +0100

    feat: add support layout

--- newspack-newsletters-templates.php
+++ newspack-newsletters-templates.php
@@ -79,3 +79,18 @@ add_filter(
 	10,
 	2
 );
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/support.json'), true ); //phpcs:ignore
+		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
+		$templates[] = [
+			'content' => $content,
+			'title'   => __( 'Support', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
