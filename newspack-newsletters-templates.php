424b55095979a8fe398d60e779bb8d15a63e4b06
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-04-16 17:56:13 +0100

    feat: add new layouts: daily/weekly (no image), breaking news, breaking news (no image), and support (#48)
    * feat: add new one column (no image) layout
    
    * feat: add breaking news layouts
    
    * feat: add support layout
    
    * feat: update support layout with first name tag

--- newspack-newsletters-templates.php
+++ newspack-newsletters-templates.php
@@ -23,11 +23,71 @@ add_filter(
 add_filter(
 	'newspack_newsletters_templates',
 	function( $templates ) {
- 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/template-1.json'), true ); //phpcs:ignore
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/daily-weekly.json'), true ); //phpcs:ignore
 		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
 		$templates[] = [
 			'content' => $content,
-			'title'   => __( 'Template 1', 'newspack-newsletters' ),
+			'title'   => __( 'Daily/Weekly', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/daily-weekly-no-image.json'), true ); //phpcs:ignore
+		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
+		$templates[] = [
+			'content' => $content,
+			'title'   => __( 'Daily/Weekly (no image)', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/breaking-news.json'), true ); //phpcs:ignore
+		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
+		$templates[] = [
+			'content' => $content,
+			'title'   => __( 'Breaking News', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/breaking-news-no-image.json'), true ); //phpcs:ignore
+		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
+		$templates[] = [
+			'content' => $content,
+			'title'   => __( 'Breaking News (no image)', 'newspack-newsletters' ),
+		];
+		return $templates;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_newsletters_templates',
+	function( $templates ) {
+ 		$decode      = json_decode( file_get_contents( NEWSPACK_NEWSLETTERS_PLUGIN_FILE . 'src/templates/support.json'), true ); //phpcs:ignore
+		$content     = Newspack_Newsletters::template_token_replacement( $decode['content'] );
+		$templates[] = [
+			'content' => $content,
+			'title'   => __( 'Support', 'newspack-newsletters' ),
 		];
 		return $templates;
 	},
