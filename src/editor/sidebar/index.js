ce6d7676b4408dce9c376dc21ff61a3da2c5b5d9
Jefferson Rabb <jeff@atavist.net>
2020-04-23 11:08:04 -0400

    fix: remove unnecessary check

--- src/editor/sidebar/index.js
+++ src/editor/sidebar/index.js
@@ -209,10 +209,7 @@ class Sidebar extends Component {
 					<Button
 						isLink
 						onClick={ () => this.updateSender( senderName, senderEmail ) }
-						disabled={
-							inFlight ||
-							( senderEmail && senderEmail.length ? ! hasValidEmail( senderEmail ) : false )
-						}
+						disabled={ inFlight || ( senderEmail.length ? ! hasValidEmail( senderEmail ) : false ) }
 					>
 						{ __( 'Update Sender', 'newspack-newsletters' ) }
 					</Button>
