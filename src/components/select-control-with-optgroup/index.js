741b778279312e0ac2d2a02cf1c81e4e39e0fc49
Jefferson Rabb <jeff@atavist.net>
2020-06-04 12:16:25 -0400

    fix: return null if optgroups is empty

--- src/components/select-control-with-optgroup/index.js
+++ src/components/select-control-with-optgroup/index.js
@@ -36,40 +36,42 @@ export default function SelectControlWithOptGroup( {
 
 	// Disable reason: A select with an onchange throws a warning
 
+	if ( isEmpty( optgroups ) ) {
+		return null;
+	}
+
 	/* eslint-disable jsx-a11y/no-onchange */
 	return (
-		! isEmpty( optgroups ) && (
-			<BaseControl
-				label={ label }
-				hideLabelFromVision={ hideLabelFromVision }
+		<BaseControl
+			label={ label }
+			hideLabelFromVision={ hideLabelFromVision }
+			id={ id }
+			help={ help }
+			className={ className }
+		>
+			<select
 				id={ id }
-				help={ help }
-				className={ className }
+				className="components-select-control__input"
+				onChange={ onChangeValue }
+				aria-describedby={ !! help ? `${ id }__help` : undefined }
+				multiple={ multiple }
+				{ ...props }
 			>
-				<select
-					id={ id }
-					className="components-select-control__input"
-					onChange={ onChangeValue }
-					aria-describedby={ !! help ? `${ id }__help` : undefined }
-					multiple={ multiple }
-					{ ...props }
-				>
-					{ optgroups.map( ( { label: optgroupLabel, options }, optgroupIndex ) => (
-						<optgroup label={ optgroupLabel } key={ optgroupIndex }>
-							{ options.map( ( option, optionIndex ) => (
-								<option
-									key={ `${ option.label }-${ option.value }-${ optionIndex }` }
-									value={ option.value }
-									disabled={ option.disabled }
-								>
-									{ option.label }
-								</option>
-							) ) }
-						</optgroup>
-					) ) }
-				</select>
-			</BaseControl>
-		)
+				{ optgroups.map( ( { label: optgroupLabel, options }, optgroupIndex ) => (
+					<optgroup label={ optgroupLabel } key={ optgroupIndex }>
+						{ options.map( ( option, optionIndex ) => (
+							<option
+								key={ `${ option.label }-${ option.value }-${ optionIndex }` }
+								value={ option.value }
+								disabled={ option.disabled }
+							>
+								{ option.label }
+							</option>
+						) ) }
+					</optgroup>
+				) ) }
+			</select>
+		</BaseControl>
 	);
 	/* eslint-enable jsx-a11y/no-onchange */
 }
