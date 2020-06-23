07d02461a38db4d885fd9225a421fcb99babaf66
Adam Borowski <adam@adamboro.com>
2020-06-23 18:31:45 +0200

    feat: lock ad saving if no expiry date is set

--- src/ads-admin/editor/index.js
+++ src/ads-admin/editor/index.js
@@ -4,12 +4,38 @@
 import { __ } from '@wordpress/i18n';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
-import { Fragment } from '@wordpress/element';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { Fragment, useEffect } from '@wordpress/element';
+import { PluginDocumentSettingPanel, PluginPrePublishPanel } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
-import { DatePicker } from '@wordpress/components';
+import { DatePicker, Notice } from '@wordpress/components';
+import { format, isInTheFuture } from '@wordpress/date';
 
-const AdEdit = ( { expiryDate, editPost } ) => {
+const AdEdit = ( { expiryDate, editPost, lockPostSaving, unlockPostSaving } ) => {
+	// Lock post saving if expiry date is not set
+	useEffect(() => {
+		if ( expiryDate ) {
+			unlockPostSaving( 'no-expiry-lock' );
+		} else {
+			lockPostSaving( 'no-expiry-lock' );
+		}
+	}, [ expiryDate ]);
+
+	const noticeProps = {
+		children: __( 'Set an expiry date to publish the ad.', 'newspack-newsletters' ),
+		status: 'error',
+	};
+	if ( expiryDate ) {
+		const formattedExpiryDate = format( 'M j Y', expiryDate );
+		const isExpiryInTheFuture = isInTheFuture( expiryDate );
+		noticeProps.children = isExpiryInTheFuture
+			? `${ __( 'This ad will expire on ', 'newspack-newsletters' ) } ${ formattedExpiryDate }.`
+			: __(
+					'The expiration date is set in the past. This ad will not be displayed.',
+					'newspack-newsletters'
+			  );
+		noticeProps.status = isExpiryInTheFuture ? 'info' : 'warning';
+	}
+
 	return (
 		<Fragment>
 			<PluginDocumentSettingPanel
@@ -21,6 +47,9 @@ const AdEdit = ( { expiryDate, editPost } ) => {
 					onChange={ expiry_date => editPost( { meta: { expiry_date } } ) }
 				/>
 			</PluginDocumentSettingPanel>
+			<PluginPrePublishPanel>
+				<Notice isDismissible={ false } { ...noticeProps } />
+			</PluginPrePublishPanel>
 		</Fragment>
 	);
 };
@@ -32,8 +61,8 @@ const AdEditWithSelect = compose( [
 		return { expiryDate: meta.expiry_date };
 	} ),
 	withDispatch( dispatch => {
-		const { editPost } = dispatch( 'core/editor' );
-		return { editPost };
+		const { editPost, lockPostSaving, unlockPostSaving } = dispatch( 'core/editor' );
+		return { editPost, lockPostSaving, unlockPostSaving };
 	} ),
 ] )( AdEdit );
 
