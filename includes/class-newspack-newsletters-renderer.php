eacb5f73d2993c8d6fd8019ab2a9c4fa37f2327a
Adam Boro <adam@adamboro.com>
2020-07-07 18:53:41 +0200

    feat(ads): allow no expiry date on ads (#252)
    Closes #251

--- includes/class-newspack-newsletters-renderer.php
+++ includes/class-newspack-newsletters-renderer.php
@@ -571,21 +571,21 @@ final class Newspack_Newsletters_Renderer {
 				array(
 					'post_type'      => Newspack_Newsletters_Ads::NEWSPACK_NEWSLETTERS_ADS_CPT,
 					'posts_per_page' => -1,
-					'meta_query'     => array( // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
-						array(
-							'key'     => 'expiry_date',
-							'value'   => gmdate( 'Y-m-d' ),
-							'compare' => '>=',
-							'type'    => 'DATE',
-						),
-					),
 				)
 			);
 			$ads        = $ads_query->get_posts();
 			$ads_markup = '';
 			foreach ( $ads as $ad ) {
 				$expiry_date = new DateTime( get_post_meta( $ad->ID, 'expiry_date', true ) );
-				$ads_markup .= self::post_to_mjml_components( $ad, false );
+				if ( ! $expiry_date ) {
+					$ads_markup .= self::post_to_mjml_components( $ad, false );
+				} else {
+					$now_date    = gmdate( 'Y-m-d' );
+					$expiry_date = $expiry_date->format( 'Y-m-d' );
+					if ( $expiry_date >= $now_date ) {
+						$ads_markup .= self::post_to_mjml_components( $ad, false );
+					}
+				}
 			}
 			$body .= $ads_markup;
 		}
--- src/ads-admin/utils.js
+++ src/ads-admin/utils.js
@@ -4,10 +4,13 @@
 import { isInTheFuture, date } from '@wordpress/date';
 
 export const isAdActive = ad => {
-	if ( ad.status === 'publish' && ad.meta.expiry_date ) {
-		const adDayDate = date( 'Y-m-d', ad.meta.expiry_date );
-		const todayDate = date( 'Y-m-d' );
-		return adDayDate === todayDate || isInTheFuture( adDayDate );
+	if ( ad.status === 'publish' ) {
+		if ( ad.meta.expiry_date ) {
+			const adDayDate = date( 'Y-m-d', ad.meta.expiry_date );
+			const todayDate = date( 'Y-m-d' );
+			return adDayDate === todayDate || isInTheFuture( adDayDate );
+		}
+		return true;
 	}
 	return false;
 };
