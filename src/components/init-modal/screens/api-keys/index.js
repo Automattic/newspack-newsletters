5d93a06f62fb3d1af4ed1d44c98781dd85ab1eb3
Laurel Fulford <laurel.fulford@automattic.com>
2020-08-06 17:55:01 -0700

    feat: adding bare-bones fields for the service provider picker

--- src/components/init-modal/screens/api-keys/index.js
+++ src/components/init-modal/screens/api-keys/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
-import { Button, ExternalLink, Spinner, TextControl } from '@wordpress/components';
+import { Button, ExternalLink, SelectControl, Spinner, TextControl } from '@wordpress/components';
 import { Fragment, useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { ENTER } from '@wordpress/keycodes';
@@ -41,6 +41,10 @@ export default ( { onSetupStatus } ) => {
 		if ( 'newspack_rest_forbidden' === error.code ) {
 			setInFlight( false );
 			setErrors( {
+				newspack_newsletters_invalid_keys_constantcontact: __(
+					'Only administrators can set Constant Contact API keys.',
+					'newspack-newsletters'
+				),
 				newspack_newsletters_invalid_keys_mailchimp: __(
 					'Only administrators can set Mailchimp API keys.',
 					'newspack-newsletters'
@@ -72,49 +76,114 @@ export default ( { onSetupStatus } ) => {
 	}, []);
 
 	const {
+		service_provider: serviceProvider = 'mailchimp',
 		mailchimp_api_key: mailchimpAPIKey = '',
+		constant_contact_api_key: constantContactAPIKey = '',
 		mjml_api_key: mjmlApplicationId = '',
 		mjml_api_secret: mjmlAPISecret = '',
 	} = keys;
 	const canSubmit =
-		mailchimpAPIKey.length > 0 && mjmlApplicationId.length > 0 && mjmlAPISecret.length > 0;
+		( ( serviceProvider === 'mailchimp' && mailchimpAPIKey.length > 0 ) ||
+			( serviceProvider === 'constant_contact' && constantContactAPIKey > 0 ) ) &&
+		mjmlApplicationId.length > 0 &&
+		mjmlAPISecret.length > 0;
 	const classes = classnames(
 		'newspack-newsletters-modal__content',
 		'newspack-newsletters-modal__settings',
 		inFlight && 'newspack-newsletters-modal__in-flight'
 	);
+
 	return (
 		<Fragment>
 			<div className={ classes }>
 				<div className="newspack-newsletters-modal__settings-wrapper">
 					{ inFlight && <Spinner /> }
-					<h4>{ __( 'Enter your Mailchimp API key', 'newspack-newsletters' ) }</h4>
-					<TextControl
-						label={ __( 'Mailchimp API key', 'newspack-newsletters' ) }
-						value={ mailchimpAPIKey }
-						onChange={ value => setKeys( { ...keys, mailchimp_api_key: value } ) }
+					<h4>{ __( 'Select your newsletter service provider', 'newspack-newsletters' ) }</h4>
+					<SelectControl
+						label={ __( 'Service Provider', 'newspack-newsletters' ) }
+						value={ serviceProvider }
 						disabled={ inFlight }
-						onKeyDown={ event => {
-							if ( canSubmit && ENTER === event.keyCode ) {
-								event.preventDefault();
-								commitSettings();
-							}
-						} }
-						className={ errors.newspack_newsletters_invalid_keys_mailchimp && 'has-error' }
+						onChange={ value => setKeys( { ...keys, service_provider: value } ) }
+						options={ [
+							{ value: '', label: __( 'Select service provider', 'newspack-newsletters' ) },
+							{ value: 'mailchimp', label: __( 'MailChimp', 'newspack-newsletters' ) },
+							{
+								value: 'constant_contact',
+								label: __( 'Constant Contact', 'newspack-newsletters' ),
+							},
+						] }
 					/>
-					{ errors.newspack_newsletters_invalid_keys_mailchimp && (
-						<p className="error">{ errors.newspack_newsletters_invalid_keys_mailchimp }</p>
-					) }
-					<p>
-						<ExternalLink href="https://us1.admin.mailchimp.com/account/api/">
-							{ __( 'Generate Mailchimp API key', 'newspack-newsletters' ) }
-						</ExternalLink>
-						<span className="separator"> | </span>
-						<ExternalLink href="https://mailchimp.com/help/about-api-keys/">
-							{ __( 'About Mailchimp API keys', 'newspack-newsletters' ) }
-						</ExternalLink>
-					</p>
 					<hr />
+					{ 'mailchimp' === serviceProvider && (
+						<Fragment>
+							<h4>{ __( 'Enter your Mailchimp API key', 'newspack-newsletters' ) }</h4>
+							<TextControl
+								label={ __( 'Mailchimp API key', 'newspack-newsletters' ) }
+								value={ mailchimpAPIKey }
+								onChange={ value => setKeys( { ...keys, mailchimp_api_key: value } ) }
+								disabled={ inFlight }
+								onKeyDown={ event => {
+									if ( canSubmit && ENTER === event.keyCode ) {
+										event.preventDefault();
+										commitSettings();
+									}
+								} }
+								className={ errors.newspack_newsletters_invalid_keys_mailchimp && 'has-error' }
+							/>
+							{ errors.newspack_newsletters_invalid_keys_mailchimp && (
+								<p className="error">{ errors.newspack_newsletters_invalid_keys_mailchimp }</p>
+							) }
+
+							<p>
+								<ExternalLink href="https://us1.admin.mailchimp.com/account/api/">
+									{ __( 'Generate Mailchimp API key', 'newspack-newsletters' ) }
+								</ExternalLink>
+								<span className="separator"> | </span>
+								<ExternalLink href="https://mailchimp.com/help/about-api-keys/">
+									{ __( 'About Mailchimp API keys', 'newspack-newsletters' ) }
+								</ExternalLink>
+							</p>
+							<hr />
+						</Fragment>
+					) }
+					{ 'constant_contact' === serviceProvider && (
+						<Fragment>
+							<h4>{ __( 'Enter your Constant Contact API key', 'newspack-newsletters' ) }</h4>
+							<TextControl
+								label={ __( 'Constant Contact API key', 'newspack-newsletters' ) }
+								value={ constantContactAPIKey }
+								onChange={ value => setKeys( { ...keys, constant_contact_api_key: value } ) }
+								disabled={ inFlight }
+								onKeyDown={ event => {
+									if ( canSubmit && ENTER === event.keyCode ) {
+										event.preventDefault();
+										commitSettings();
+									}
+								} }
+								className={
+									errors.newspack_newsletters_invalid_keys_constantcontact && 'has-error'
+								}
+							/>
+							{ errors.newspack_newsletters_invalid_keys_constantcontact && (
+								<p className="error">
+									{ errors.newspack_newsletters_invalid_keys_constantcontact }
+								</p>
+							) }
+
+							<p>
+								{ /* TODO: Get correct link to generate Constant Contact API key. */ }
+								<ExternalLink href="#">
+									{ __( 'Generate Constant Contact API key', 'newspack-newsletters' ) }
+								</ExternalLink>
+								<span className="separator"> | </span>
+								{ /* TODO: Get correct link to information about Constant Contact API keys. */ }
+								<ExternalLink href="https://developer.constantcontact.com/v2-api-keys.html">
+									{ __( 'About Constant Contact API keys', 'newspack-newsletters' ) }
+								</ExternalLink>
+							</p>
+							<hr />
+						</Fragment>
+					) }
 					<h4>{ __( 'Enter your MJML API keys', 'newspack-newsletters' ) }</h4>
 					<TextControl
 						label={ __( 'MJML application ID', 'newspack-newsletters' ) }
