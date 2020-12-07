93deff495ad46f9755f903e2ba366371ebb4a471
Jefferson Rabb <jeff@atavist.net>
2020-12-07 14:07:33 -0500

    feat: setting to opt-in to mailchimp tags

--- includes/service-providers/mailchimp/class-newspack-newsletters-mailchimp.php
+++ includes/service-providers/mailchimp/class-newspack-newsletters-mailchimp.php
@@ -172,8 +172,10 @@ final class Newspack_Newsletters_Mailchimp extends \Newspack_Newsletters_Service
 				}
 			}
 
+			$newspack_newsletters_use_mailchimp_tags = get_option( 'newspack_newsletters_use_mailchimp_tags', false );
+
 			$tags = [];
-			if ( $list_id ) {
+			if ( $list_id && $newspack_newsletters_use_mailchimp_tags ) {
 				$tags_response = $this->validate(
 					$mc->get( "lists/$list_id/segments?count=1000", [], 20 ),
 					__( 'Error retrieving Mailchimp tags.', 'newspack_newsletters' )
