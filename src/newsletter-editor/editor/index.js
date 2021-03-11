3a720a6b4a4a73edbb62df701bf24ea8f8a4bab7
Adam Borowski <adam@adamboro.com>
2021-03-11 16:20:10 +0100

    fix: colors of headings with links; top block space in editor

--- src/newsletter-editor/editor/index.js
+++ src/newsletter-editor/editor/index.js
@@ -129,10 +129,13 @@ const Editor = compose( [
 	}, [ props.status ]);
 
 	useEffect(() => {
-		// Unhide post title if the newsletter is a public post.
-		document.querySelector( '.editor-post-title' ).style.display = props.isPublic
-			? 'initial'
-			: 'none';
+		// Hide post title if the newsletter is a not a public post.
+		const editorTitleEl = document.querySelector( '.editor-post-title' );
+		if ( editorTitleEl ) {
+			editorTitleEl.classList[ props.isPublic ? 'remove' : 'add' ](
+				'newspack-newsletters-post-title-hidden'
+			);
+		}
 	}, [ props.isPublic ]);
 
 	return createPortal( <SendButton />, publishEl );
