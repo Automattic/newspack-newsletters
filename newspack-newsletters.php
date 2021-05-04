214a752bccc7dd1757b62e6b4a82c2f10de2fbe4
matticbot <semantic-release-bot@martynus.net>
2021-05-04 15:09:30 +0000

    chore(release): 1.25.0-alpha.1 [skip ci]
    # [1.25.0-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v1.24.1...v1.25.0-alpha.1) (2021-05-04)
    
    ### Features
    
    * custom CSS ([#454](https://github.com/Automattic/newspack-newsletters/issues/454)) ([748247c](https://github.com/Automattic/newspack-newsletters/commit/748247c14a1c3ca22bdf5d041162f00ddbda9f0a))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -6,7 +6,7 @@
  * Author:          Automattic
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         1.24.1
+ * Version:         1.25.0-alpha.1
  *
  * @package         Newspack_Newsletters
  */
