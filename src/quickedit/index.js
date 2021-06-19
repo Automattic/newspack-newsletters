25bb4026165904d74efd26fdba2bbf6bdff32af1
krupaly2k <krupaly2k@gmail.com>
2021-06-20 00:23:44 +0530

    Bulk action #issue 325 PHPCS issues resolution

--- src/quickedit/index.js
+++ src/quickedit/index.js
@@ -1,32 +1,32 @@
-jQuery(function($){
+const jQuery = window && window.jQuery;
 
+jQuery( function( $ ) {
 	let inlineEditPost;
-	let wp_inline_edit_function = inlineEditPost.edit;
+	const wp_inline_edit_function = inlineEditPost.edit;
 
 	inlineEditPost.edit = function( post_id ) {
-
 		// let's merge arguments of the original function
 		wp_inline_edit_function.apply( this, arguments );
 
 		// get the post ID from the argument
 		let id = 0;
-		if ( typeof( post_id ) == 'object' ) { // if it is object, get the ID number
+		if ( typeof post_id === 'object' ) {
+			// if it is object, get the ID number
 			id = parseInt( this.getId( post_id ) );
 		}
 
 		//if post id exists
 		if ( id > 0 ) {
 			// add rows to variables
-			let specific_post_edit_row = $('#edit-' + id),
-				specific_post_row = $('#post-' + id),
-				public_val = false; // let's say by default checkbox is unchecked
+
+			const specific_post_edit_row = $( '#edit-' + id ),
+				specific_post_row = $( '#post-' + id );
 
 			// check if the Published as post column says Yes
-			if( $( '.column-public', specific_post_row ).text() == 'Yes' ) public_val = true;
-
+			if ( $( '.column-public', specific_post_row ).text() === 'Yes' ) {
+				$( ':input[name="npublic"]', specific_post_edit_row ).prop( 'checked', true );
+			}
 			// populate the inputs with column data
-			
-			$( ':input[name="npublic"]', specific_post_edit_row ).prop('checked', public_val );
 		}
-	}
-});
+	};
+} );
