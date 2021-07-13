d6e52e3ac218051ae60cd9f929cba5488a20d97a
Miguel Peixe <miguel@peixe.co>
2021-07-13 13:59:15 -0300

    fix: check manual provider before credentials

--- includes/class-newspack-newsletters.php
+++ includes/class-newspack-newsletters.php
@@ -926,16 +926,18 @@ final class Newspack_Newsletters {
 		}
 
 		// Service Provider credentials.
-		if ( empty( $credentials ) ) {
-			$wp_error->add(
-				'newspack_newsletters_invalid_keys',
-				__( 'Please input credentials.', 'newspack-newsletters' )
-			);
-		} else {
-			$status = self::$provider->set_api_credentials( $credentials );
-			if ( is_wp_error( $status ) ) {
-				foreach ( $status->errors as $code => $message ) {
-					$wp_error->add( $code, implode( ' ', $message ) );
+		if ( 'manual' !== $service_provider ) {
+			if ( empty( $credentials ) ) {
+				$wp_error->add(
+					'newspack_newsletters_invalid_keys',
+					__( 'Please input credentials.', 'newspack-newsletters' )
+				);
+			} else {
+				$status = self::$provider->set_api_credentials( $credentials );
+				if ( is_wp_error( $status ) ) {
+					foreach ( $status->errors as $code => $message ) {
+						$wp_error->add( $code, implode( ' ', $message ) );
+					}
 				}
 			}
 		}
