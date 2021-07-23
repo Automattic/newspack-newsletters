de78e133a79f52721f2dd3ea7b8ba95728d86815
Thomas Guillot <info@thomasguillot.com>
2021-07-23 15:25:13 +0100

    feat: add wordpress icons package and update share block icon

--- src/editor/blocks/share/index.js
+++ src/editor/blocks/share/index.js
@@ -3,26 +3,29 @@
  */
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { Icon, customLink } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
-import Icon from './icon';
 import edit from './edit';
 import save from './save';
 import { SHARE_BLOCK_NAME } from './consts';
 
 export default () => {
 	registerBlockType( SHARE_BLOCK_NAME, {
-		title: 'Share Newsletter',
+		title: __( 'Share Newsletter', 'newspack-newsletters' ),
 		category: 'text',
-		icon: Icon,
+		icon: <Icon icon={ customLink } />,
 		attributes: {
 			content: {
 				type: 'string',
 				source: 'html',
 				selector: 'p',
-				default: __( 'Thanks for reading. If you liked it, please send to a friend!', 'newspack' ),
+				default: __(
+					'Thanks for reading. If you liked it, please send to a friend!',
+					'newspack-newsletters'
+				),
 				__experimentalRole: 'content',
 			},
 			shareMessage: {
