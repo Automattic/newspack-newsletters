2b046e09e42e13ab381ec24811be959ced8323ec
matticbot <semantic-release-bot@martynus.net>
2021-07-29 23:05:22 +0000

    chore(release): 1.30.2 [skip ci]
    ## [1.30.2](https://github.com/Automattic/newspack-newsletters/compare/v1.30.1...v1.30.2) (2021-07-29)
    
    ### Bug Fixes
    
    * composer dependency bug ([0d0dc20](https://github.com/Automattic/newspack-newsletters/commit/0d0dc20d6c8377c6bd38ce2e1398ea7abc9dcce3))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -6,7 +6,7 @@
  * Author:          Automattic
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         1.30.1
+ * Version:         1.30.2
  *
  * @package         Newspack_Newsletters
  */
