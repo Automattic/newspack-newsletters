d33c8e042e94042ceb87ab931fe4915a39b25d01
Miguel Peixe <miguel@peixe.co>
2021-08-30 17:50:43 -0300

    feat: more coverage and add ability for "supportedTags"

--- src/editor/blocks/mailchimp-merge-tags/index.js
+++ src/editor/blocks/mailchimp-merge-tags/index.js
@@ -20,6 +20,96 @@ const tags = [
 		),
 	},
 	{
+		tag: '*|CAMPAIGN_UID|*',
+		label: __( 'Displays the unique ID for your campaign.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|REWARDS|*',
+		label: __( 'Adds the Referral badge to your campaign.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|REWARDS_TEXT|*',
+		label: __( 'Adds a text-only version of the Rewards link.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|MC:TRANSLATE|*',
+		label: __(
+			'Inserts links to translate your sent campaign into different languages.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|TRANSLATE:xx|*',
+		label: __(
+			"Adds a list of links to translate the content in your campaign. Replace xx with the code for the language your campaign is written in, and we'll display other available languages.",
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|MC_LANGUAGE|*',
+		label: __(
+			"Displays the language code for a particular subscriber. For example, if your subscriber's language is set to English, the merge tag output will display the code en.",
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|MC_LANGUAGE_LABEL|*',
+		label: __(
+			'Displays the plain-text language for a particular subscriber. All languages are written English, so if your subscriber\'s language is set to German we\'ll display "German" instead of Deutsch.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|DATE:X|*',
+		label: __(
+			'Use to show the current date in a given format. Replace X with the format of your choice.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|LIST:RECENTX|*',
+		label: __(
+			'Displays a list of links to recent campaigns sent to the audience indicated. Replace X with the number of campaigns to show.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|MC:TOC|*',
+		label: __( 'Creates a linked table of contents in your campaign.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|MC:TOC_TEXT|*',
+		label: __(
+			'Creates a table of contents in your campaigns as plain-text.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|MC_PREVIEW_TEXT|*',
+		label: __(
+			'Use this merge tag to generate preview text in a custom-coded campaign.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|POLL:RATING:x|* *|END:POLL|*',
+		label: __( 'Creates a poll to record subscriber ratings of 1-10.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|SURVEY|* *|END:|*',
+		label: __(
+			'Creates a one-question survey with a set number of responses that subscribers can choose from.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|PROMO_CODE:[$store_id=x, $rule_id=x, $code_id=x]|*',
+		label: __(
+			'Use this tag to include a promo code in a campaign. Replace the “x” variables in your Promo Code merge tag to specify what promo code to display.',
+			'newspack-newsletters'
+		),
+	},
+	{
 		tag: '*|FNAME|*',
 		label: __(
 			"Inserts your subscriber's first name if it's available in your audience.",
@@ -36,6 +126,70 @@ const tags = [
 		keywords: [ 'last name' ],
 	},
 	{
+		tag: '*|EMAIL|*',
+		label: __( "Inserts your subscriber's email address.", 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|PHONE|*',
+		label: __(
+			'Inserts your subscriber’s phone number if it’s available in your audience.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|ADDRESS|*',
+		label: __(
+			'Inserts your subscriber’s address if it’s available in your audience.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|LIST:NAME|*',
+		label: __( 'Inserts the name of your audience.', 'newspack-newsletters' ),
+	},
+	{
+		tag: '*|LIST:COMPANY|*',
+		label: __(
+			"Inserts the name of your company or organization that's listed in the required email footer content for your audience.",
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|LIST:SUBSCRIBERS|*',
+		label: __(
+			'Inserts the number of subscribers in your audience in plain-text.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|USER:COMPANY|*',
+		label: __(
+			'Inserts the company or organization name listed under Primary Account Contact info in your Mailchimp account.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|MC:DATE|*',
+		label: __(
+			'Displays MM/DD/YYYY or DD/MM/YYYY based on your settings in your account Details.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|CURRENT_YEAR|*',
+		label: __(
+			'Displays the current year. This is great if you include a copyright date in your campaign, because it will update automatically every year.',
+			'newspack-newsletters'
+		),
+	},
+	{
+		tag: '*|UNSUB|*',
+		label: __(
+			"Gives your subscribers the opportunity to unsubscribe from your emails. (Required by law and Mailchimp's Terms of Use.).",
+			'newspack-newsletters'
+		),
+	},
+	{
 		tag: '*|TWITTER:PROFILE|*',
 		label: __(
 			"Populates your campaign with your Twitter avatar, follower, tweet, and following counts; and a follow link. Doesn't include your latest tweets.",
@@ -45,12 +199,20 @@ const tags = [
 ];
 
 /**
+ * Supported merge tags.
+ * Empty array means all merge tags are supported.
+ */
+const supportedTags = [];
+
+/**
  * Merge tags completer configuration.
  */
 const completer = {
 	name: 'Mailchimp Merge Tags',
 	triggerPrefix: '*|',
-	options: tags,
+	options: supportedTags.length
+		? tags.filter( tag => supportedTags.indexOf( tag.tag ) !== -1 )
+		: tags,
 	getOptionLabel: ( { tag, label } ) => (
 		<div>
 			<code>{ tag }</code>
