15b1b8a77a36349d3e3dd530dd6f52f2ae5d9715
matticbot <semantic-release-bot@martynus.net>
2021-09-14 15:38:12 +0000

    chore(release): 1.33.1-alpha.1 [skip ci]
    ## [1.33.1-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v1.33.0...v1.33.1-alpha.1) (2021-09-14)
    
    ### Bug Fixes
    
    * handle unavailable lists ([#585](https://github.com/Automattic/newspack-newsletters/issues/585)) ([c84cc77](https://github.com/Automattic/newspack-newsletters/commit/c84cc779d4d55deb5a26fbe6b3a224dd414e2533))
    * manual provider setting ([1c8811a](https://github.com/Automattic/newspack-newsletters/commit/1c8811a03635ed166007635af6086d8885a9f141))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -6,7 +6,7 @@
  * Author:          Automattic
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         1.33.0
+ * Version:         1.33.1-alpha.1
  *
  * @package         Newspack_Newsletters
  */
