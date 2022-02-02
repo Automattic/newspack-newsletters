2a5476a70161c0e8c74abccd241321f12cfee3b1
Thomas Guillot <thomasguillot@users.noreply.github.com>
2022-02-02 10:26:38 +0000

    fix: layout buttons gap

--- src/newsletter-editor/layout/index.js
+++ src/newsletter-editor/layout/index.js
@@ -173,31 +173,33 @@ export default compose( [
 						</div>
 					</div>
 				) }
-				<div className="newspack-newsletters-buttons-group newspack-newsletters-buttons-group--spaced">
+				<div className="newspack-newsletters-buttons-group">
 					<Button
-						isPrimary
+						variant="secondary"
 						disabled={ isEditedPostEmpty || isSavingLayout }
 						onClick={ () => setIsManageModalVisible( true ) }
 					>
-						{ __( 'Save new layout', 'newspack-newsletters' ) }
+						{ __( 'Save New Layout', 'newspack-newsletters' ) }
 					</Button>
 
 					{ isUsingCustomLayout && (
 						<Button
-							isSecondary
+							variant="tertiary"
 							disabled={ isPostContentSameAsLayout || isSavingLayout }
 							onClick={ handeLayoutUpdate }
 						>
-							{ __( 'Update layout', 'newspack-newsletters' ) }
+							{ __( 'Update Layout', 'newspack-newsletters' ) }
 						</Button>
 					) }
+
+					<Button
+						variant="secondary"
+						isDestructive
+						onClick={ () => setWarningModalVisible( true ) }
+					>
+						{ __( 'Reset Layout', 'newspack-newsletters' ) }
+					</Button>
 				</div>
-
-				<br />
-
-				<Button isSecondary isLink isDestructive onClick={ () => setWarningModalVisible( true ) }>
-					{ __( 'Reset newsletter layout', 'newspack-newsletters' ) }
-				</Button>
 
 				{ isManageModalVisible && (
 					<Modal
