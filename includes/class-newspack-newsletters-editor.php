2c6c05837a0b072dfd97f3c1c89fdcbbd93ba84f
Derrick Koo <derrick.koo@automattic.com>
2022-02-10 16:15:45 -0700

    fix: php warning when unregistering a block pattern that isn't registered

--- includes/class-newspack-newsletters-editor.php
+++ includes/class-newspack-newsletters-editor.php
@@ -127,7 +127,11 @@ final class Newspack_Newsletters_Editor {
 		remove_editor_styles();
 		add_theme_support( 'editor-gradient-presets', array() );
 		add_theme_support( 'disable-custom-gradients' );
-		unregister_block_pattern( 'core/social-links-shared-background-color' );
+
+		$block_type_registry = new \WP_Block_Type_Registry();
+		if ( $block_type_registry->is_registered( 'core/social-links-shared-background-color' ) ) {
+			unregister_block_pattern( 'core/social-links-shared-background-color' );
+		}
 	}
 
 	/**
