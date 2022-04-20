b1df5ef6745118dfe63865d9bb6dbb481cea80f3
Miguel Peixe <miguel.peixe@automattic.com>
2022-04-20 17:27:46 -0300

    refactor: sending newsletters

--- includes/service-providers/class-newspack-newsletters-service-provider.php
+++ includes/service-providers/class-newspack-newsletters-service-provider.php
@@ -42,6 +42,7 @@ abstract class Newspack_Newsletters_Service_Provider implements Newspack_Newslet
 		if ( $this->controller && $this->controller instanceof \WP_REST_Controller ) {
 			add_action( 'rest_api_init', [ $this->controller, 'register_routes' ] );
 		}
+		add_action( 'pre_post_update', [ $this, 'pre_post_update' ], 10, 2 );
 	}
 
 	/**
@@ -71,5 +72,74 @@ abstract class Newspack_Newsletters_Service_Provider implements Newspack_Newslet
 			);
 		}
 		return true;
+	}
+
+	/**
+	 * Handle newsletter post status changes.
+	 *
+	 * @param int   $post_id The post ID.
+	 * @param array $data    Unslashed post data.
+	 */
+	public function pre_post_update( $post_id, $data ) {
+		$post       = get_post( $post_id );
+		$old_status = $post->post_status;
+		$new_status = $data['post_status'];
+
+		// Only run if it's a newsletter post.
+		if ( ! Newspack_Newsletters::validate_newsletter_id( $post_id ) ) {
+			return;
+		}
+
+		// Only run if this is the active provider.
+		if ( Newspack_Newsletters::service_provider() !== $this->service ) {
+			return;
+		}
+
+		// Send if changing from any status to publish.
+		if ( 'publish' === $new_status && 'publish' !== $old_status ) {
+			$result = $this->send_newsletter( $post );
+			if ( is_wp_error( $result ) ) {
+				$transient = sprintf( 'newspack_newsletters_error_%s_%s', $post->ID, get_current_user_id() );
+				set_transient( $transient, $result->get_error_message(), 45 );
+				wp_die( esc_html( $result->get_error_message() ) );
+			}
+		}
+
+		// Prevent status change if newsletter has been sent.
+		if (
+			'publish' === $old_status &&
+			'publish' !== $new_status &&
+			get_post_meta( $post_id, '_newspack_newsletters_sent', true )
+		) {
+			wp_die( esc_html( __( 'You cannot change a sent newsletter status.', 'newspack-newsletters' ) ) );
+		}
+	}
+
+	/**
+	 * Send a newsletter.
+	 *
+	 * @param WP_Post $post The newsletter post.
+	 *
+	 * @return true|WP_Error True if successful, WP_Error if not.
+	 */
+	public function send_newsletter( $post ) {
+		$post_id = $post->ID;
+
+		// Newsletter was already sent.
+		if ( get_post_meta( $post_id, '_newspack_newsletters_sent', true ) ) {
+			return;
+		}
+
+		try {
+			$result = $this->send( $post );
+		} catch ( Exception $e ) {
+			$result = new WP_Error( 'newspack_newsletter_error', $e->getMessage() );
+		}
+
+		if ( true === $result ) {
+			update_post_meta( $post_id, '_newspack_newsletters_sent', time() );
+		}
+
+		return $result;
 	}
 }
