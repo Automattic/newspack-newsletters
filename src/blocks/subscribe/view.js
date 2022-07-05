555a7b50e6d853fdd7a17e00c6a95bd804c32e37
Miguel Peixe <miguel@peixe.co>
2022-07-05 12:06:00 -0300

    fix: add domReady

--- src/blocks/subscribe/view.js
+++ src/blocks/subscribe/view.js
@@ -1,41 +1,50 @@
 /**
+ * WordPress dependencies
+ */
+import domReady from '@wordpress/dom-ready';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
 
 ( function () {
-	[ ...document.querySelectorAll( '.newspack-newsletters-subscribe' ) ].forEach( container => {
-		const form = container.querySelector( 'form' );
-		if ( ! form ) {
-			return;
-		}
-		const messageContainer = container.querySelector( '.newspack-newsletters-subscribe-response' );
-		const submit = container.querySelector( 'input[type="submit"]' );
-		form.addEventListener( 'submit', ev => {
-			ev.preventDefault();
-			const body = new FormData( form );
-			if ( ! body.has( 'email' ) || ! body.get( 'email' ) ) {
+	domReady( function () {
+		[ ...document.querySelectorAll( '.newspack-newsletters-subscribe' ) ].forEach( container => {
+			const form = container.querySelector( 'form' );
+			if ( ! form ) {
 				return;
 			}
-			submit.disabled = true;
-			messageContainer.innerHTML = '';
-			fetch( form.getAttribute( 'action' ) || window.location.pathname, {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-				},
-				body,
-			} ).then( res => {
-				submit.disabled = false;
-				res.json().then( ( { message } ) => {
-					const messageNode = document.createElement( 'p' );
-					messageNode.innerHTML = message;
-					messageNode.className = `message status-${ res.status }`;
-					if ( res.status === 200 ) {
-						container.replaceChild( messageNode, form );
-					} else {
-						messageContainer.appendChild( messageNode );
-					}
+			const messageContainer = container.querySelector(
+				'.newspack-newsletters-subscribe-response'
+			);
+			const submit = container.querySelector( 'input[type="submit"]' );
+			form.addEventListener( 'submit', ev => {
+				ev.preventDefault();
+				const body = new FormData( form );
+				if ( ! body.has( 'email' ) || ! body.get( 'email' ) ) {
+					return;
+				}
+				submit.disabled = true;
+				messageContainer.innerHTML = '';
+				fetch( form.getAttribute( 'action' ) || window.location.pathname, {
+					method: 'POST',
+					headers: {
+						Accept: 'application/json',
+					},
+					body,
+				} ).then( res => {
+					submit.disabled = false;
+					res.json().then( ( { message } ) => {
+						const messageNode = document.createElement( 'p' );
+						messageNode.innerHTML = message;
+						messageNode.className = `message status-${ res.status }`;
+						if ( res.status === 200 ) {
+							container.replaceChild( messageNode, form );
+						} else {
+							messageContainer.appendChild( messageNode );
+						}
+					} );
 				} );
 			} );
 		} );
