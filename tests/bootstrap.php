f163991759b1fafe8243ba3d109dbce86145b3d0
Leo Germani <leogermani@automattic.com>
2022-09-14 15:14:31 -0300

    test: update tests dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,5 +27,8 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Load the composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
