8047c9c717e89f3f1e92b83522ef8cdbb63e8846
matticbot <semantic-release-bot@martynus.net>
2023-07-04 15:51:29 +0000

    chore(release): 1.67.2 [skip ci]
    ## [1.67.2](https://github.com/Automattic/newspack-newsletters/compare/v1.67.1...v1.67.2) (2023-07-04)
    
    ### Bug Fixes
    
    * preserve list order on subscribe block ([#1211](https://github.com/Automattic/newspack-newsletters/issues/1211)) ([e712735](https://github.com/Automattic/newspack-newsletters/commit/e71273582e2de5c3c77e45bc59c1c4cdc6f2c8b8))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         1.67.1
+ * Version:         1.67.2
  *
  * @package         Newspack_Newsletters
  */
