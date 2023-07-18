91d57c368092e2e2f4bf03b4d419720e4523d48d
Miguel Peixe <miguel.peixe@a8c.com>
2023-07-18 11:51:24 -0300

    feat: add sorting

--- includes/tracking/class-admin.php
+++ includes/tracking/class-admin.php
@@ -17,6 +17,8 @@ final class Admin {
 	public static function init() {
 		add_action( 'manage_' . \Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT . '_posts_columns', [ __CLASS__, 'manage_columns' ] );
 		add_action( 'manage_' . \Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT . '_posts_custom_column', [ __CLASS__, 'custom_column' ], 10, 2 );
+		add_action( 'manage_edit-' . \Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT . '_sortable_columns', [ __CLASS__, 'sortable_columns' ] );
+		add_action( 'pre_get_posts', [ __CLASS__, 'handle_sorting' ] );
 	}
 
 	/**
@@ -41,6 +43,40 @@ final class Admin {
 			echo intval( get_post_meta( $post_id, 'newspack_newsletters_tracking_pixel_seen', true ) );
 		} elseif ( 'clicks' === $column_name ) {
 			echo intval( get_post_meta( $post_id, 'tracking_clicks', true ) );
+		}
+	}
+
+	/**
+	 * Sortable columns.
+	 *
+	 * @param array $columns Columns.
+	 */
+	public static function sortable_columns( $columns ) {
+		$columns['opened'] = 'opened';
+		$columns['clicks'] = 'clicks';
+		return $columns;
+	}
+
+	/**
+	 * Handle sorting.
+	 *
+	 * @param \WP_Query $query Query.
+	 */
+	public static function handle_sorting( $query ) {
+		if ( ! is_admin() || ! $query->is_main_query() ) {
+			return;
+		}
+		if ( \Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT !== $query->get( 'post_type' ) ) {
+			return;
+		}
+
+		$orderby = $query->get( 'orderby' );
+		if ( 'opened' === $orderby ) {
+			$query->set( 'meta_key', 'newspack_newsletters_tracking_pixel_seen' );
+			$query->set( 'orderby', 'meta_value_num' );
+		} elseif ( 'clicks' === $orderby ) {
+			$query->set( 'meta_key', 'tracking_clicks' );
+			$query->set( 'orderby', 'meta_value_num' );
 		}
 	}
 }
