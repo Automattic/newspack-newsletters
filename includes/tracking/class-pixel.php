035f07ee78a7e8a600b4c8f23da907b1ae3094f0
Miguel Peixe <miguel.peixe@a8c.com>
2023-07-28 14:22:34 -0300

    fix: prevent prefetching and bots from triggering the pixel

--- includes/tracking/class-pixel.php
+++ includes/tracking/class-pixel.php
@@ -139,6 +139,38 @@ final class Pixel {
 			return;
 		}
 
+		// Disable caching.
+		header( 'Cache-Control: no-cache, no-store, must-revalidate' );
+		header( 'Pragma: no-cache' );
+		header( 'Expires: 0' );
+
+		// phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPressVIPMinimum.Variables.RestrictedVariables.cache_constraints___SERVER__HTTP_USER_AGENT__
+		// Skip bots.
+		if (
+			! empty( $_SERVER['HTTP_USER_AGENT'] ) &&
+			preg_match( '/bot|crawl|slurp|spider|mediapartners/i', $_SERVER['HTTP_USER_AGENT'] )
+		) {
+			return;
+		}
+		// Skip prefetching and previews.
+		if (
+			! empty( $_SERVER['HTTP_X_PURPOSE'] ) &&
+			in_array( $_SERVER['HTTP_X_PURPOSE'], [ 'preview', 'instant' ], true )
+		) {
+			return;
+		}
+		if ( ! empty( $_SERVER['HTTP_X_MOZ'] ) && 'prefetch' === $_SERVER['HTTP_X_MOZ'] ) {
+			return;
+		}
+		// Skip Google Image Pre-Fetch.
+		if (
+			! empty( $_SERVER['HTTP_USER_AGENT'] ) &&
+			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246 Mozilla/5.0' === $_SERVER['HTTP_USER_AGENT']
+		) {
+			return;
+		}
+		// phpcs:enable
+
 		// Set the appropriate content type header.
 		header( 'Content-Type: image/gif' );
 		// Output a transparent 1x1 pixel image.
