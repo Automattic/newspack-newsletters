27dce292036447bb9a4f93a4c555e302b8e2837b
Derrick Koo <dkoo@users.noreply.github.com>
2023-08-21 17:25:11 -0600

    fix: register before adding contact (#1250)

--- src/blocks/subscribe/index.php
+++ src/blocks/subscribe/index.php
@@ -376,6 +376,14 @@ function process_form() {
 		$metadata['status'] = 'pending';
 	}
 
+	if ( ! \is_user_logged_in() && \class_exists( '\Newspack\Reader_Activation' ) && \Newspack\Reader_Activation::is_enabled() ) {
+		$metadata = array_merge( $metadata, [ 'registration_method' => 'newsletters-subscription' ] );
+		if ( $popup_id ) {
+			$metadata['registration_method'] = 'newsletters-subscription-popup';
+		}
+		\Newspack\Reader_Activation::register_reader( $email, $name, true, $metadata );
+	}
+
 	$result = \Newspack_Newsletters_Subscription::add_contact(
 		[
 			'name'     => $name ?? null,
@@ -387,14 +395,6 @@ function process_form() {
 
 	if ( \is_wp_error( $result ) ) {
 		return send_form_response( $result );
-	}
-
-	if ( ! \is_user_logged_in() && \class_exists( '\Newspack\Reader_Activation' ) && \Newspack\Reader_Activation::is_enabled() ) {
-		$metadata = array_merge( $metadata, [ 'registration_method' => 'newsletters-subscription' ] );
-		if ( $popup_id ) {
-			$metadata['registration_method'] = 'newsletters-subscription-popup';
-		}
-		\Newspack\Reader_Activation::register_reader( $email, $name, true, $metadata );
 	}
 
 	/**
