5992d49a8925027cd912cffa76c839a4711f1f81
matticbot <semantic-release-bot@martynus.net>
2023-09-28 21:15:40 +0000

    chore(release): 2.1.3-alpha.1 [skip ci]
    ## [2.1.3-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v2.1.2...v2.1.3-alpha.1) (2023-09-28)
    
    ### Bug Fixes
    
    * prevent duplicate js plugin registration ([#1298](https://github.com/Automattic/newspack-newsletters/issues/1298)) ([11efaa7](https://github.com/Automattic/newspack-newsletters/commit/11efaa7841374e0b8abe52a44bbe77f30f645e23))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         2.1.2
+ * Version:         2.1.3-alpha.1
  *
  * @package         Newspack_Newsletters
  */
