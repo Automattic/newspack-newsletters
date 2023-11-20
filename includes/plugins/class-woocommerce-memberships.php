e66dff46a0f83eac68a13d551cc0378cf85fe3c8
Leo Germani <leogermani@automattic.com>
2023-11-20 10:44:55 -0300

    fix: do not auto subscribe to premium lists
    also avoid fatal on unsubscribe

--- includes/plugins/class-woocommerce-memberships.php
+++ includes/plugins/class-woocommerce-memberships.php
@@ -29,14 +29,14 @@ class Woocommerce_Memberships {
 	 */
 	protected static $user_id_in_scope;
 
-	/** 
+	/**
 	 * Initialize the class
 	 */
 	public static function init() {
 		add_action( 'plugins_loaded', [ __CLASS__, 'init_hooks' ] );
 	}
-	
-	/** 
+
+	/**
 	 * Initialize the hooks after all plugins are loaded
 	 */
 	public static function init_hooks() {
@@ -48,7 +48,6 @@ class Woocommerce_Memberships {
 		add_filter( 'newspack_newsletters_manage_newsletters_available_lists', [ __CLASS__, 'filter_lists_objects' ] );
 		add_filter( 'newspack_auth_form_newsletters_lists', [ __CLASS__, 'filter_lists_objects' ] );
 		add_action( 'wc_memberships_user_membership_status_changed', [ __CLASS__, 'remove_user_from_list' ], 10, 3 );
-		add_action( 'wc_memberships_user_membership_saved', [ __CLASS__, 'add_user_to_list' ], 10, 2 );
 		add_action( 'wc_memberships_user_membership_deleted', [ __CLASS__, 'deleted_membership' ] );
 	}
 
@@ -81,7 +80,7 @@ class Woocommerce_Memberships {
 
 				return wc_memberships_user_can( $user_id, 'view', [ 'post' => $list_object->get_id() ] );
 
-			} 
+			}
 		);
 		return array_values( $lists );
 	}
@@ -124,14 +123,18 @@ class Woocommerce_Memberships {
 		if ( in_array( $new_status, $status_considered_active ) ) {
 			return;
 		}
-		
+
 		$lists_to_remove = [];
 		$user            = $user_membership->get_user();
 		if ( ! $user ) {
 			return;
 		}
 		$user_email = $user->user_email;
-		$rules      = $user_membership->get_plan()->get_content_restriction_rules();
+		$plan       = $user_membership->get_plan();
+		if ( ! is_a( $plan, 'WC_Memberships_Membership_Plan' ) ) {
+			return;
+		}
+		$rules = $plan->get_content_restriction_rules();
 		foreach ( $rules as $rule ) {
 			if ( Subscription_Lists::CPT !== $rule->get_content_type_name() ) {
 				continue;
@@ -149,74 +152,7 @@ class Woocommerce_Memberships {
 		$provider = Newspack_Newsletters::get_service_provider();
 		$provider->update_contact_lists_handling_local( $user_email, [], $lists_to_remove );
 		Newspack_Newsletters_Logger::log( 'Reader ' . $user_email . ' removed from the following lists: ' . implode( ', ', $lists_to_remove ) );
-		
-	}
 
-	/**
-	 * Adds user to premium lists when a membership is granted
-	 *
-	 * @param \WC_Memberships_Membership_Plan $plan the plan that user was granted access to.
-	 * @param array                           $args 
-	 * {
-	 *     Array of User Membership arguments.
-	 * 
-	 *     @type int $user_id the user ID the membership is assigned to.
-	 *     @type int $user_membership_id the user membership ID being saved.
-	 *     @type bool $is_update whether this is a post update or a newly created membership.
-	 * }
-	 * @return void
-	 */
-	public static function add_user_to_list( $plan, $args ) {
-
-		// When creating the membership via admin panel, this hook is called once before the membership is actually created.
-		if ( ! $plan instanceof \WC_Memberships_Membership_Plan ) {
-			return;
-		}
-
-		$status_considered_active = wc_memberships()->get_user_memberships_instance()->get_active_access_membership_statuses();
-
-		$user_membership = new \WC_Memberships_User_Membership( $args['user_membership_id'] );
-
-		if ( ! in_array( $user_membership->get_status(), $status_considered_active, true ) ) {
-			return;
-		}
-
-		$user_id = $args['user_id'] ?? false;
-		if ( ! $user_id ) {
-			return;
-		}
-		$user = get_user_by( 'ID', $user_id );
-		if ( ! $user ) {
-			return;
-		}
-
-		self::$user_id_in_scope = $user_id;
-
-		$lists_to_add = [];
-		$user_email   = $user->user_email;
-		$rules        = $plan->get_content_restriction_rules();
-
-		Newspack_Newsletters_Logger::log( 'New membership granted to ' . $user_email );
-
-		foreach ( $rules as $rule ) {
-			if ( Subscription_Lists::CPT !== $rule->get_content_type_name() ) {
-				continue;
-			}
-			$object_ids = $rule->get_object_ids();
-			foreach ( $object_ids as $object_id ) {
-				$subscription_list = new Subscription_List( $object_id );
-				$lists_to_add[]    = $subscription_list->get_form_id();
-			}
-		}
-		
-		if ( empty( $lists_to_add ) ) {
-			return;
-		}
-
-		$provider = Newspack_Newsletters::get_service_provider();
-		$provider->update_contact_lists_handling_local( $user_email, $lists_to_add );
-		Newspack_Newsletters_Logger::log( 'Reader ' . $user_email . ' added to the following lists: ' . implode( ', ', $lists_to_add ) );
-		
 	}
 
 	/**
