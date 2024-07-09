dac977203c93a167cb86302720888235b5939012
matticbot <semantic-release-bot@martynus.net>
2024-07-09 18:46:27 +0000

    chore(release): 2.20.0-alpha.2 [skip ci]
    # [2.20.0-alpha.2](https://github.com/Automattic/newspack-newsletters/compare/v2.20.0-alpha.1...v2.20.0-alpha.2) (2024-07-09)
    
    ### Bug Fixes
    
    * nonexistent method, and don't remove the submit button (just the spinner) ([#1565](https://github.com/Automattic/newspack-newsletters/issues/1565)) ([406c2f4](https://github.com/Automattic/newspack-newsletters/commit/406c2f49bc8b3366687460ff0eb1568566a7d2eb))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         2.20.0-alpha.1
+ * Version:         2.20.0-alpha.2
  *
  * @package         Newspack_Newsletters
  */
