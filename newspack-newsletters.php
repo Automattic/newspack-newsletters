776f316b0fcbb594a40fbbc11b36e09406dfdcf5
matticbot <semantic-release-bot@martynus.net>
2024-08-13 15:04:47 +0000

    chore(release): 3.0.0 [skip ci]
    # [3.0.0](https://github.com/Automattic/newspack-newsletters/compare/v2.21.2...v3.0.0) (2024-08-13)
    
    ### Bug Fixes
    
    * check for valid ID before post type check ([#1610](https://github.com/Automattic/newspack-newsletters/issues/1610)) ([96e21bc](https://github.com/Automattic/newspack-newsletters/commit/96e21bce7cb5beba199144a473fabb6c4169eb10))
    * update dependencies to support `@wordpress/scripts` ([#1530](https://github.com/Automattic/newspack-newsletters/issues/1530)) ([2d732f2](https://github.com/Automattic/newspack-newsletters/commit/2d732f21b192ad9151167f93a543c755cd2fa4a1))
    
    ### Features
    
    * **campaign-monitor:** deprecation warning and environment constant for CM ([#1579](https://github.com/Automattic/newspack-newsletters/issues/1579)) ([e0374ac](https://github.com/Automattic/newspack-newsletters/commit/e0374ac025bcbb9cdcf3f706c45e4ce31a64ee81))
    
    ### BREAKING CHANGES
    
    * Updates dependencies for compatibility with WordPress 6.6.*, but breaks JS in WordPress 6.5.* and below. If you need support for WP 6.5.*, please do not upgrade to this new major version.
    
    * chore: refactor for updating dependencies in newspack-scripts
    
    * refactor: use proxy script for eslint and stylelint scripts
    
    * fix: peer dependency conflicts
    
    * chore: update newspack-scripts to v5.6.0-alpha.3
    
    * chore: remove ajv dependency
    
    * chore: update lint-staged
    
    * chore: add .stylelintrc.js
    
    * chore: update newspack-scripts to v5.6.0-alpha.4
    
    * fix: package-lock.json
    
    * fix: reformat SCSS
    
    * fix: add missing Prettier config files
    
    * chore: update newspack-scripts to 5.6.0-alpha.5
    
    * chore: update newspack-scripts to v5.6.0-alpha.7
    
    * chore: update newspack-scripts to v5.6.0-alpha.8
    
    * chore: add NPM scripts for PHP
    
    * chore: update newspack-components to v3.0.0
    
    * chore: bump newspack-scripts to v5.5.2

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.0.0-alpha.2
+ * Version:         3.0.0
  *
  * @package         Newspack_Newsletters
  */
