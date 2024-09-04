350046e850a505e521f938eeab87bb9a429181aa
matticbot <semantic-release-bot@martynus.net>
2024-09-04 07:59:55 +0000

    chore(release): 3.1.1 [skip ci]
    ## [3.1.1](https://github.com/Automattic/newspack-newsletters/compare/v3.1.0...v3.1.1) (2024-09-04)
    
    ### Bug Fixes
    
    * error handling, tests, CI builds ([#1641](https://github.com/Automattic/newspack-newsletters/issues/1641), [#1642](https://github.com/Automattic/newspack-newsletters/issues/1642)) ([791fdc3](https://github.com/Automattic/newspack-newsletters/commit/791fdc3f8dc2c32ccafe7acea0413acf1440c1bd))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.1.0
+ * Version:         3.1.1
  *
  * @package         Newspack_Newsletters
  */
