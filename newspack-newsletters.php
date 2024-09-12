1b4a6c603fd0bfd3a200ea51f88a139dac0272fc
matticbot <semantic-release-bot@martynus.net>
2024-09-12 16:54:59 +0000

    chore(release): 3.1.3 [skip ci]
    ## [3.1.3](https://github.com/Automattic/newspack-newsletters/compare/v3.1.2...v3.1.3) (2024-09-12)
    
    ### Bug Fixes
    
    * active campaign usage report, fetch campaigns in batches ([#1649](https://github.com/Automattic/newspack-newsletters/issues/1649)) ([e8a4155](https://github.com/Automattic/newspack-newsletters/commit/e8a415534f7a2db0c7a308c47792d9583af7cc0a))

--- newspack-newsletters.php
+++ newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.1.2
+ * Version:         3.1.3
  *
  * @package         Newspack_Newsletters
  */
