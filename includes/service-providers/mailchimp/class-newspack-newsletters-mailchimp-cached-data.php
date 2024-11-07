6d7602efcd53867f92bddaddc1f1847b2f6b2628
dkoo <derrick.koo@automattic.com>
2024-11-07 11:52:24 -0700

    fix: properly show errors from trying to refresh cache

--- includes/service-providers/mailchimp/class-newspack-newsletters-mailchimp-cached-data.php
+++ includes/service-providers/mailchimp/class-newspack-newsletters-mailchimp-cached-data.php
@@ -288,10 +288,14 @@ final class Newspack_Newsletters_Mailchimp_Cached_Data {
 	/**
 	 * Clears the cache errors for a given list
 	 *
-	 * @param string $list_id The List ID.
+	 * @param string|null $list_id The List ID, or null for all lists.
 	 * @return void
 	 */
-	private static function clear_errors( $list_id ) {
+	private static function clear_errors( $list_id = null ) {
+		if ( ! $list_id ) {
+			delete_option( self::ERRORS_OPTION );
+			return;
+		}
 		$errors = get_option( self::ERRORS_OPTION, [] );
 		if ( isset( $errors[ $list_id ] ) ) {
 			unset( $errors[ $list_id ] );
@@ -303,11 +307,16 @@ final class Newspack_Newsletters_Mailchimp_Cached_Data {
 	/**
 	 * Stores the last error for a given list, if the cache is older than self::SURFACE_ERRORS_AFTER
 	 *
-	 * @param string $list_id The List ID.
-	 * @param string $error The error message.
+	 * @param string|null $list_id The List ID, or null for all lists.
+	 * @param string      $error The error message.
 	 */
-	private static function maybe_add_error( $list_id, $error ) {
-		Newspack_Newsletters_Logger::log( 'Mailchimp cache: handling error while fetching cache for list ' . $list_id );
+	private static function maybe_add_error( $list_id = null, $error ) {
+		Newspack_Newsletters_Logger::log(
+			sprintf(
+				'Mailchimp cache: handling error while fetching cache for %s',
+				$list_id ? 'list ' . $list_id : 'all lists'
+			)
+		);
 		$cache_date = get_option( self::get_cache_date_key( $list_id ) );
 		if ( $cache_date && ( time() - $cache_date ) > self::SURFACE_ERRORS_AFTER ) {
 			$errors             = get_option( self::ERRORS_OPTION, [] );
@@ -528,6 +537,7 @@ final class Newspack_Newsletters_Mailchimp_Cached_Data {
 		Newspack_Newsletters_Logger::log( 'Mailchimp cache: Invalidating cached data' );
 		delete_option( self::get_cache_date_key() );
 		delete_option( self::get_lists_cache_key() );
+		self::clear_errors();
 	}
 
 	/**
@@ -541,6 +551,7 @@ final class Newspack_Newsletters_Mailchimp_Cached_Data {
 			$lists = self::fetch_lists(); // Force a cache refresh.
 		} catch ( Exception $e ) {
 			Newspack_Newsletters_Logger::log( 'Mailchimp cache: Error refreshing lists cache: ' . $e->getMessage() );
+			self::maybe_add_error( null, $e->getMessage() );
 			return;
 		}
 
