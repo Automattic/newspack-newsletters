0cbb39f4c2aa41eecacc1bd88a677605867affc7
dkoo <derrick.koo@automattic.com>
2025-03-07 15:54:55 -0700

    fix: add utm_source and utm_campaign params to newsletter links

--- includes/class-newspack-newsletters-renderer.php
+++ includes/class-newspack-newsletters-renderer.php
@@ -398,8 +398,20 @@ final class Newspack_Newsletters_Renderer {
 	 */
 	public static function process_links( $html, $post = null ) {
 		preg_match_all( '/href="([^"]*)"/', $html, $matches );
-		$href_params = $matches[0];
-		$urls        = $matches[1];
+		$href_params   = $matches[0];
+		$urls          = $matches[1];
+		$provider      = Newspack_Newsletters::get_service_provider();
+		$campaign_name = $post ? $provider->get_campaign_name( $post ) : false;
+		$send_list_id  = $post ? get_post_meta( $post->ID, 'send_list_id', true ) : false;
+		$utm_params    = [
+			'utm_medium' => 'email',
+		];
+		if ( $campaign_name ) {
+			$utm_params['utm_campaign'] = $campaign_name;
+		}
+		if ( $send_list_id ) {
+			$utm_params['utm_source'] = $send_list_id;
+		}
 		foreach ( $urls as $index => $url ) {
 			/** Skip if link was already processed. */
 			if ( ! empty( self::$processed_links[ $url ] ) ) {
@@ -412,9 +424,7 @@ final class Newspack_Newsletters_Renderer {
 			$url_with_params = apply_filters(
 				'newspack_newsletters_process_link',
 				add_query_arg(
-					[
-						'utm_medium' => 'email',
-					],
+					$utm_params,
 					$url
 				),
 				$url,
