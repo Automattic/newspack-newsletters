7cb1dae38b4cfe35ee975348bf82bd68dd8fae6f
Adam Cassis <adam.cassis@automattic.com>
2025-04-15 12:22:59 +0200

    fix(editor): ads config call

--- includes/class-newspack-newsletters-ads.php
+++ includes/class-newspack-newsletters-ads.php
@@ -345,7 +345,7 @@ final class Newspack_Newsletters_Ads {
 	public static function get_ads_config( $request ) {
 		$letterhead                 = new Newspack_Newsletters_Letterhead();
 		$has_letterhead_credentials = $letterhead->has_api_credentials();
-		$post_id                    = $request->get_param( 'id' );
+		$post_id                    = $request->get_param( 'postId' );
 		$newspack_ad_type           = self::CPT;
 
 		$url_to_manage_promotions   = 'https://app.tryletterhead.com/promotions';
