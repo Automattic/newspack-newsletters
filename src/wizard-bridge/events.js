b69f432e250b73b655bc2079697020131a7ff365
Thomas Guillot <thomas@automattic.com>
2026-05-07 10:46:59 +0100

    feat(admin): add wizard-bridge event-name constants

---
+++ src/wizard-bridge/events.js
@@ -0,0 +1,9 @@
+export const EVENT_NAMESPACE = 'newspack-newsletters';
+
+export const EVENTS = {
+	BRIDGE_MOUNTED: `${ EVENT_NAMESPACE }:bridge-mounted`,
+	OPEN_MODAL: `${ EVENT_NAMESPACE }:open-local-list-modal`,
+	OPEN_CONFIRM_DELETE: `${ EVENT_NAMESPACE }:open-local-list-confirm-delete`,
+	LOCAL_LIST_SAVED: `${ EVENT_NAMESPACE }:local-list-saved`,
+	LOCAL_LIST_DELETED: `${ EVENT_NAMESPACE }:local-list-deleted`,
+};
