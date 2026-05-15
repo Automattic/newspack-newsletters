77740bb924580e12946079c9b899a3e0e393bf3c
Thomas Guillot <thomas@automattic.com>
2026-05-15 10:00:33 +0100

    style(editor): set merge-tag picker offset to 12px (NEWS-2244)

--- src/editor/blocks/merge-tags/toolbar-button.js
+++ src/editor/blocks/merge-tags/toolbar-button.js
@@ -33,7 +33,7 @@ const MergeTagPicker = ( { anchor, onSelect, onClose } ) => {
 			anchor={ anchor }
 			className="newspack-newsletters-merge-tags-picker__popover"
 			placement="bottom-start"
-			offset={ 16 }
+			offset={ 12 }
 			focusOnMount="firstElement"
 			onClose={ onClose }
 			onFocusOutside={ onClose }
