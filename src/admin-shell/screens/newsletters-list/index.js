8f5a37752ba963a7f2b41569916bed89a01640d6
Thomas Guillot <thomas@automattic.com>
2026-05-15 14:07:31 +0100

    feat(admin-shell): hide categories/tags columns by default (NEWS-1932)

--- src/admin-shell/screens/newsletters-list/index.js
+++ src/admin-shell/screens/newsletters-list/index.js
@@ -34,7 +34,7 @@ const DEFAULT_VIEW = {
 	search: '',
 	filters: [],
 	titleField: 'title',
-	fields: [ 'status', 'send_date', 'send_list', 'author', 'categories', 'tags', 'public_page', 'date' ],
+	fields: [ 'status', 'send_date', 'send_list', 'author', 'public_page', 'date' ],
 	...getInitialView(),
 };
 
